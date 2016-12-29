fa51255941fab3a4c9b23440d1b0cc2416cbd36f
Gian Sebastian <giantcrabi@gmail.com>
2016-12-30 04:36:16 +0700

    Update Paket JO

--- application/controllers/Endorsement/Paket.php
+++ application/controllers/Endorsement/Paket.php
@@ -11,6 +11,8 @@ class Paket extends MY_Controller {
     $this->load_sidebar();
     $this->load->model('Perlindungan/Agency_model');
     $this->load->model('Perlindungan/Pptkis_model');
+    $this->load->model('Endorsement/Paket_model');
+
     $this->data['listdp'] = $this->listdp;
     $this->data['usedpg'] = $this->usedpg;
     $this->data['usedmpg'] = $this->usedmpg;
@@ -43,6 +45,79 @@ class Paket extends MY_Controller {
     $this->load->view('templates/headerendorsement', $this->data);
     $this->load->view('endorsement/DaftarPaketJO_view');
     $this->load->view('templates/footerendorsement');
+  }
+
+  public function getSisa()
+  {
+    $jobd = $this->input->get('jobdid', TRUE);
+    $jpid = $this->input->get('jpid', TRUE);
+
+    $row = $this->Paket_model->getJobOrder($jobd);
+    $agid = $row[0]->agid;
+    $ppkode = $row[0]->ppkode;
+    $jobtglawalnya = $row[0]->jobtglawal;
+    $jobtglakhirnya = $row[0]->jobtglakhir;
+
+    $i = 0;
+    $query = $this->Paket_model->getJobDetail($jpid,$agid,$ppkode,$jobtglawalnya,$jobtglakhirnya);
+    foreach ($query as $row):
+      $i++;
+      $jobid[$i] = $row->jobid;
+      $jobdid[$i] = $row->jobdid;
+      $jobtglawal[$i] = $row->jobtglawal;
+      $jobtglakhir[$i] = $row->jobtglakhir;
+      $jobdl[$i] = $row->jobdl;
+      $jobdp[$i] = $row->jobdp;
+      $jobdc[$i] =  $row->jobdc;
+    endforeach;
+    
+    $prevdate = '0000-00-00';
+    $j = 0;
+    $query = $this->Paket_model->getDate($jpid,$agid,$ppkode,$jobtglawalnya,$jobtglakhirnya);
+    foreach ($query as $row):
+      $curdate = $row->date;
+      if($curdate != $prevdate)
+      {
+        $j++;
+        $period[$j] = $row->date;
+      }
+      $prevdate = $curdate;
+    endforeach;
+
+    for($k = 1; $k < $j; $k++)
+    {
+      $start = $period[$k];
+      $end = $period[$k+1];
+      $lriil = $this->Paket_model->getEntryJOLaki($start,$end,$jpid,$agid,$ppkode);
+      $priil = $this->Paket_model->getEntryJOPerempuan($start,$end,$jpid,$agid,$ppkode);
+      for($l = 1; $l <= $i; $l++)
+      {
+        if($jobtglawal[$l] <= $start && $jobtglakhir[$l] >= $end)
+        {
+          $lavail = $jobdl[$l];
+          if($lavail < $lriil) { $jobdl[$l] = 0; $lriil -= $lavail;}
+          else {$jobdl[$l] -= $lriil; $lriil = 0;}
+          $pavail = $jobdp[$l];
+          if($pavail < $priil) { $jobdp[$l] = 0; $priil -= $pavail;}
+          else {$jobdp[$l] -= $priil; $priil = 0;}
+          if($lriil > 0){
+            $cavail = $jobdc[$l];
+            if($cavail < $lriil) { $jobdc[$l] = 0; $lriil -= $cavail;}
+            else {$jobdc[$l] -= $lriil; $lriil = 0;}
+          }
+          if($priil > 0){
+            $cavail = $jobdc[$l];
+            if($cavail < $priil) { $jobdc[$l] = 0; $priil -= $cavail;}
+            else {$jobdc[$l] -= $priil; $priil = 0;}
+          }
+        }
+      }
+    }
+
+    $sisa[0] = $jobdl[$i];
+    $sisa[1] = $jobdp[$i];
+    $sisa[2] = $jobdc[$i];
+    return $sisa;
   }
 
   // AJAX AUTOCOMPLETE
--- application/models/Endorsement/Paket_model.php
+++ application/models/Endorsement/Paket_model.php
@@ -1,4 +1,102 @@
 <?php
 class Paket_model extends CI_Model {
+	public function __construct()
+    {
+        $this->load->database('default');
+    }
+
+    function getJobOrder($jobdid)
+    {
+    	$this->db->select('jo.*');
+    	$this->db->from('jodetail, jo');
+    	$this->db->where('jodetail.jobid = jo.jobid');
+    	$this->db->where('jo.jobenable = 1');
+    	$this->db->where('jodetail.jobdid', $jobdid);
+    	$result = $this->db->get()->result();
+
+    	return $result;
+    }
+
+    function getJobDetail($jpid,$agid,$ppkode,$jobtglawalnya,$jobtglakhirnya)
+    {
+    	$this->db->select('jo.jobid, jobdid, jobtglawal, jobtglakhir, jobdl, jobdp, jobdc');
+    	$this->db->from('jodetail, jo');
+    	$this->db->where('jodetail.jobid = jo.jobid');
+    	$this->db->where('jo.agid', $agid);
+    	$this->db->where('jo.ppkode', $ppkode);
+    	$this->db->where('jodetail.idjenispekerjaan', $jpid);
+    	$this->db->where('jo.jobenable = 1');
+    	$this->db->where("jo.jobtglawal <= '".$jobtglawalnya."' and jo.jobtglakhir <= '".$jobtglakhirnya."'");
+    	$this->db->group_by('jobid');
+    	$this->db->order_by('jobtglakhir ASC, jobtglawal ASC');
+    	$result = $this->db->get()->result();
+
+    	return $result;
+    }
+
+    function getDate($jpid,$agid,$ppkode,$jobtglawalnya,$jobtglakhirnya)
+    {
+    	$this->db->distinct();
+    	$this->db->select('jobtglawal AS date');
+    	$this->db->from('jodetail, jo');
+    	$this->db->where('jodetail.jobid = jo.jobid');
+    	$this->db->where('jo.agid', $agid);
+    	$this->db->where('jo.ppkode', $ppkode);
+    	$this->db->where('jodetail.idjenispekerjaan', $jpid);
+    	$this->db->where('jo.jobenable = 1');
+    	$this->db->where("jo.jobtglakhir <= '".$jobtglakhirnya."'");
+    	$query1 = $this->db->get_compiled_select();
+
+    	$this->db->distinct();
+    	$this->db->select('jobtglakhir AS date');
+    	$this->db->from('jodetail, jo');
+    	$this->db->where('jodetail.jobid = jo.jobid');
+    	$this->db->where('jo.agid', $agid);
+    	$this->db->where('jo.ppkode', $ppkode);
+    	$this->db->where('jodetail.idjenispekerjaan', $jpid);
+    	$this->db->where('jo.jobenable = 1');
+    	$this->db->where("jo.jobtglakhir <= '".$jobtglakhirnya."'");
+    	$query2 = $this->db->get_compiled_select();
+
+    	$query = $this->db->query("(".$query1.") UNION (".$query2.") ORDER BY date ASC");
+
+    	return $query->result();
+    }
+
+    function getEntryJOLaki($start,$end,$jpid,$agid,$ppkode)
+    {
+		$this->db->select('tkid');
+		$this->db->from('tki, entryjo');
+		$this->db->where('tki.ejid = entryjo.ejid');
+		$this->db->where('tkrevid IS NULL');
+		$this->db->where("tkjk like 'L'");
+		$this->db->where("entryjo.ejtglendorsement >= '" . $start . "'");
+		$this->db->where("entryjo.ejtglendorsement < '" . $end . "'");
+		$this->db->where('entryjo.agid', $agid);
+		$this->db->where('entryjo.ppkode', $ppkode);
+		$this->db->where('entryjo.idjenispekerjaan', $jpid);
+
+		$num = $this->db->count_all_results();
+
+  		return $num;
+    }
+
+    function getEntryJOPerempuan($start,$end,$jpid,$agid,$ppkode)
+    {
+		$this->db->select('tkid');
+		$this->db->from('tki, entryjo');
+		$this->db->where('tki.ejid = entryjo.ejid');
+		$this->db->where('tkrevid IS NULL');
+		$this->db->where("tkjk like 'P'");
+		$this->db->where("entryjo.ejtglendorsement >= '" . $start . "'");
+		$this->db->where("entryjo.ejtglendorsement < '" . $end . "'");
+		$this->db->where('entryjo.agid', $agid);
+		$this->db->where('entryjo.ppkode', $ppkode);
+		$this->db->where('entryjo.idjenispekerjaan', $jpid);
+
+		$num = $this->db->count_all_results();
+
+  		return $num;
+    }
 
 }

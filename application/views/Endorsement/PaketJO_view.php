c4fdafc1b0720aa7e5b6ce4c2f43b4bed720884c
Gian Sebastian <giantcrabi@gmail.com>
2017-01-03 00:43:42 +0700

    Update View PK

--- application/views/Endorsement/PaketJO_view.php
+++ application/views/Endorsement/PaketJO_view.php
@@ -14,28 +14,6 @@
         </div>
         <div class="x_content">
           <br />
-          <div class="form-group">
-            <label class="control-label col-md-2 col-sm-2 col-xs-12" for="name">Nama Agensi <span class="required">*</span></label>
-            <div class="col-md-5 col-sm-5 col-xs-12">
-              <input type="text" id="agensi" name="name" required="required" class="form-control">
-            </div>
-          </div><br /><br /><br />
-
-          <div class="form-group">
-            <label class="control-label col-md-2 col-sm-2 col-xs-12" for="name">Nama PPTKIS <span class="required">*</span></label>
-            <div class="col-md-5 col-sm-5 col-xs-12">
-              <input type="text" id="pptkis" name="name" required="required" class="form-control">
-            </div>
-          </div><br /><br /><br />
-
-        <div class="ln_solid"></div>
-        <div class="form-group">
-          <div class="col-md-8 col-sm-8 col-xs-12">
-            <button id="reset" type="reset" class="btn btn-primary">Cancel</button>
-            <button id="pilih" type="submit" class="btn btn-success">Submit</button>
-          </div>
-        </div><br /><br /><br />
-
         <div class="col-md-12 col-sm-12 col-xs-12">
           <div id="jo" class="row">
             <table id="grid"></table>

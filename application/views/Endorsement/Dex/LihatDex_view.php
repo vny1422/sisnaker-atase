b2686c43f0503df0c572debd61fc3d8a702551ce
Thiar Hasbiya <thiar.hasbiya@gmail.com>
2018-01-25 21:41:13 +0700

    add datatable server side

--- application/views/Endorsement/Dex/LihatDex_view.php
+++ application/views/Endorsement/Dex/LihatDex_view.php
@@ -22,9 +22,58 @@
           </div>
           </div>' ?>
         <?php endif; ?>
+        <table id="table" class="display" cellspacing="0" width="100%">
+            <thead>
+                <tr>
+                  <th>Nama Pekerja</th>
+                  <th>No Paspor</th>
+                  <th>Nama Majikan</th>
+                  <th>Nama Perusahaan</th>
+                  <th>Jenis Pekerjaan</th>
+                  <th>Tanggal Mulai</th>
+                  <th>Tanggal Selesai</th>
+                  <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
       </div>
 
     </div>
     <br />
   </div>
 </div>
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.13/js/dataTables.bootstrap.min.js"></script>
+<script type="text/javascript">
+    var save_method; //for save method string
+    var table;
+
+    $(document).ready(function() {
+        //datatables
+        table = $('#table').DataTable({ 
+            "processing": true, //Feature control the processing indicator.
+            "serverSide": true, //Feature control DataTables' server-side processing mode.
+            "order": [], //Initial no order.
+            // Load data for the table's content from an Ajax source
+            "ajax": {
+                "url": '<?php echo base_url()?>/dex/entry_json',
+                "type": "POST"
+            },
+            //Set column definition initialisation properties.
+            "columns": [
+                {"data": "nama_pekerja",width:100},
+                {"data": "no_paspor",width:100},
+                {"data": "nama_majikan",width:100},
+                {"data": "nama_perusahaan",width:100},
+                {"data": "pekerjaan",width:100},
+                {"data": "tgl_mulai",width:100},
+                {"data": "tgl_selesai",width:100},
+                {"data": "status",width:170}
+            ],
+
+        });
+    });
+</script>

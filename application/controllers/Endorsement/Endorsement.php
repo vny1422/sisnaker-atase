6f6f04f76a370315f4b665c8dac1d0da85e95c4d
ovn <setyassida@gmail.com>
2018-03-05 11:31:57 +0700

    View Agensi

--- application/controllers/Endorsement/Endorsement.php
+++ application/controllers/Endorsement/Endorsement.php
@@ -72,6 +72,15 @@ class Endorsement extends MY_Controller {
     $this->load->view('templates/footerendorsement');
   }
 
+  public function listagensi()
+	{
+		$this->data['title'] = 'View Agensi';
+		$this->data['subtitle'] = 'View Agensi';
+		$this->load->view('templates/headerperlindungan', $this->data);
+		$this->load->view('Endorsement/ListAgency_view', $this->data);
+		$this->load->view('templates/footerperlindungan');
+	}
+
   public function get_info_year_dashboard(){
     $year = $this->input->post('y');
     $month = $this->input->post('m');
@@ -326,7 +335,7 @@ class Endorsement extends MY_Controller {
     $this->data['subtitle2'] = 'Cetak Stiker';
     $this->load->view('templates/headerendorsement', $this->data);
     $this->load->view('Endorsement/PrintStiker_view', $this->data);
-    $this->load->view('templates/footerendorsement');    
+    $this->load->view('templates/footerendorsement');
   }
 
   public function cetakStikerEJ()
@@ -341,7 +350,7 @@ class Endorsement extends MY_Controller {
     $this->data['subtitle2'] = 'Cetak Stiker Job Order';
     $this->load->view('templates/headerendorsement', $this->data);
     $this->load->view('Endorsement/PrintStikerEJ_view', $this->data);
-    $this->load->view('templates/footerendorsement');    
+    $this->load->view('templates/footerendorsement');
   }
 
   public function getKukodeByBC()

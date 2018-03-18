b9a3b4234236f59d6a2f2219f4dcb54e386a8ea0
Budi Pang <budi.pangestu@bukalapak.com>
2018-03-19 01:24:11 +0700

    minus dex and perlintasan

--- application/controllers/RestfulAPI.php
+++ application/controllers/RestfulAPI.php
@@ -15,19 +15,16 @@ class RestfulAPI extends REST_Controller
 
 	public function getJO_get()
 	{
-		$this->load->model('Endorsement/JO_model');
 		$agid = $this->get('agid');
 		$ppkode = $this->get('ppkode');
 		if($agid != NULL && $ppkode != NULL)
 		{
-			$jo = $this->JO_model->api_get_data_jo_by_agensi_and_pptkis($agid, $ppkode);
-
-			$c = 0;
+			$jo = $this->API_model->getJOByAgidPpkode($agid, $ppkode);
 
 			foreach ($jo as $key) {
-				$jodetail = $this->JO_model->api_get_data_jo_detail_by_jobid($key->jobid);
+				$jodetail = $this->API_model->getJODetail($key->jobid);
 				$key->detail = $jodetail;
-				}
+			}
 			if($jo)
 			{
 				$this->response($jo, REST_Controller::HTTP_OK);
@@ -50,6 +47,10 @@ class RestfulAPI extends REST_Controller
 			if($date != NULL)
 			{
 				$jo = $this->API_model->getJOByDate($date);
+				foreach ($jo as $key) {
+					$jodetail = $this->API_model->getJODetail($key->jobid);
+					$key->detail = $jodetail;
+				}
 				if($jo)
 				{
 					$this->response($jo, REST_Controller::HTTP_OK);
@@ -74,7 +75,6 @@ class RestfulAPI extends REST_Controller
 				if($barcode != NULL)
 				{
 					$entryjo = $this->EntryJO_model->api_get_entry_jo_by_barcode($barcode);
-					//var_dump($entryjo);
 
 					if($entryjo)
 					{
@@ -101,7 +101,6 @@ class RestfulAPI extends REST_Controller
 					if($barcode != NULL)
 					{
 						$entryjo = $this->EntryJO_model->api_get_entry_jo_by_barcode($barcode);
-						//var_dump($entryjo);
 
 						if($entryjo)
 						{
@@ -119,5 +118,107 @@ class RestfulAPI extends REST_Controller
 						}
 					}
 
+				public function getPKByDate_get($date)
+				{
+					ini_set('max_execution_time', 600);
+
+					$date = $this->get('date');
+					if($date != NULL)
+					{
+						$entryjo = $this->API_model->getPKByDate($date);
+						foreach ($entryjo as $key) {
+							$tkilist = $this->API_model->getTKI($key->ejid);
+							$key->tki = $tkilist;
+						}
+
+						if($entryjo)
+						{
+							$this->response($entryjo, REST_Controller::HTTP_OK);
+						}
+						else {
+							$this->response([[
+								'status' => FALSE,
+								'message' => 'Entry JO not found.'
+								]], REST_Controller::HTTP_BAD_REQUEST);
+							}
+						}
+					else {
+						$this->response(NULL, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+						}
+					}
+
+					public function getBlacklistAgenByDate_get($date)
+					{
+						$date = $this->get('date');
+						if($date != NULL)
+						{
+							$blacklists = $this->API_model->getBlacklistAgenByDate($date);
+							$list = array();
+							foreach($blacklists as $key)
+							{
+								array_push($list, $key->agid);
+							}
+
+							$object = (object) [
+						    'list_agid' => $list
+						  ];
+
+							if($object)
+							{
+								$this->response($object, REST_Controller::HTTP_OK);
+							}
+							else {
+								$this->response([[
+									'status' => FALSE,
+									'message' => 'Agency not found.'
+									]], REST_Controller::HTTP_BAD_REQUEST);
+								}
+							}
+						else {
+							$this->response(NULL, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+							}
+					}
+
+					public function pushKeberangkatan_post()
+					{
+						if($this->post())
+						{
+								$response = $this->API_model->pushKeberangkatan($this->post());
+
+								if($response != 0) {
+									$this->response($response, REST_Controller::HTTP_CREATED); // OK (200) being the HTTP response code
+								}
+								else {
+									$this->response([[
+										'status' => FALSE,
+										'message' => 'Post Data invalid.'
+										]], REST_Controller::HTTP_BAD_REQUEST);
+								}
+						}
+						else {
+							$this->response(NULL, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+						}
+					}
+
+					public function pushKepulangan_post()
+					{
+						if($this->post())
+						{
+								$response = $this->API_model->pushKepulangan($this->post());
+
+								if($response != 0) {
+									$this->response($response, REST_Controller::HTTP_CREATED); // OK (200) being the HTTP response code
+								}
+								else {
+									$this->response([[
+										'status' => FALSE,
+										'message' => 'Post Data invalid.'
+										]], REST_Controller::HTTP_BAD_REQUEST);
+								}
+						}
+						else {
+							$this->response(NULL, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+						}
+					}
 
 				}
